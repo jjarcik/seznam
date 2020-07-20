a1bdf22e403662537a3e2a9f9fb7fc5a55e20480
Jan Jarčík <jan.jarcik@gmail.com>
2020-07-20 11:46:07 +0200

    Add style for product list component

--- src/components/blocks/ProductListItem/index.tsx
+++ src/components/blocks/ProductListItem/index.tsx
@@ -1,14 +1,16 @@
 import React, { FC } from 'react'
 import { Link } from 'react-router-dom'
 import { ProductListItemType } from './types'
-import style from './ProductListItem.module.css'
+import styles from './ProductListItem.module.css'
 
 const ProductListItem: FC<ProductListItemType> = ({ id, name, image, price, perex }: ProductListItemType) => (
-  <article className={style.base}>
-    <img src={image} alt={name} className={style.image} />
+  <article className={styles.base}>
+    <img src={image} alt={name} className={styles.image} />
     <div>
-      <Link to={`/detail/${id}`}>{name}</Link>
-      <span className={style.price}>{price}</span>
+      <Link to={`/detail/${id}`} className={styles.name}>
+        {name}
+      </Link>
+      <span className={styles.price}>{price}</span>
       <p>{perex}</p>
     </div>
   </article>
--- src/components/elements/Button/index.tsx
+++ src/components/elements/Button/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Button.module.css'
 
 interface ButtonProps {
   text: string
 }
 
-const Button: React.FC<ButtonProps> = ({ text }: ButtonProps) => <button>{text}</button>
+const Button: React.FC<ButtonProps> = ({ text }: ButtonProps) => <button className={styles.base}>{text}</button>
 
 export default Button
