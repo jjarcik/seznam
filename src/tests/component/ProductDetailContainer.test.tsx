36fe272d1268b9db4b133a6fd252273286ac3ebc
Jan Jarčík <jan.jarcik@gmail.com>
2020-07-21 10:11:18 +0200

    new structure of test file

--- src/tests/component/ProductDetailContainer.test.tsx
+++ src/tests/component/ProductDetailContainer.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { screen } from '@testing-library/react'
-import { renderWithRedux } from './helpers'
-import ProductDetail from '../components/blocks/ProductDetail'
-import api from '../api.json'
+import { renderWithRedux } from '../helpers'
+import ProductDetail from '../../components/blocks/ProductDetail'
+import api from '../../api.json'
 
-test('display text loading while fetching data from API', () => {
+test('with redux - display text "loading" while fetching data from API', () => {
   const { getByText } = renderWithRedux(<ProductDetail id={1} />)
   expect(getByText(/loading/)).toBeInTheDocument()
 })
 
-test('render product detail with data from API', () => {
+test('with redux - render product detail with data from API', () => {
   const initialReduxState = {
     products: {
       items: api.products,
